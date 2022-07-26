0cdfceb0f5a6791c911d242d9aea5c2ed2820a13
Sharad Jadhav <sharad.jadhav.8551@gmail.com>
2022-07-26 21:07:15 +0530

    MUI icons added

--- src/Layouts/App/index.tsx
+++ src/Layouts/App/index.tsx
@@ -1,8 +1,10 @@
 import { Outlet } from "react-router-dom";
+import CssBaseline from "@mui/material/CssBaseline";
 
 export default function AppLayout() {
   return (
     <>
+      <CssBaseline />
       <header></header>
       <main>
         <Outlet />
