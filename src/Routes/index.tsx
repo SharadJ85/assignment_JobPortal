ec123ee0a0252997acd9e9b79a347e5855db0424
Sharad Jadhav <sharad.jadhav.8551@gmail.com>
2022-07-26 20:40:54 +0530

    app layout set

--- src/Routes/index.tsx
+++ src/Routes/index.tsx
@@ -5,6 +5,7 @@ import {
   Route,
 } from "react-router-dom";
 import { routePaths } from "../api/routePaths";
+import AppLayout from "../Layouts/App";
 import HomePage from "../Pages/Home";
 import JobPage from "../Pages/Job";
 import NotFound from "../Pages/NotFound";
@@ -13,7 +14,10 @@ export default function AppRoutes() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path={routePaths.root}>
+        <Route
+          path={routePaths.root}
+          element={<AppLayout />}
+        >
           <Route index element={<HomePage />} />
           <Route path={routePaths.job}>
             <Route
